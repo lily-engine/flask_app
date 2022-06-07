595bacd4ff502f205a47bdbb8aa48134d7a50ea0
kikuchi <nyoronyoro0327@gmail.com>
2022-06-07 23:55:09 +0900

    Create base4.py

---
+++ section-3/basic/base4.py
@@ -0,0 +1,14 @@
+# 数値型
+
+value = 1
+print(value)
+value = -2
+print(value)
+
+value = value + 4 #2
+print(value)
+print(value * 4) #8
+print(value / 3) #0.66666
+value = 10
+print(value // 3) #3
+print(value % 3) #1
