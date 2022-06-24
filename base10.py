5841f2e9b5373dd15b3392877b196701807df85f
kikuchi <nyoronyoro0327@gmail.com>
2022-06-24 23:58:38 +0900

    0624

--- base10.py
+++ base10.py
@@ -7,6 +7,13 @@ print(car.get('brand')) # dict.get('key') # keyが存在しなかった場合は
 
 print(car.get(1)) # 文字列の1ではなく数値の1に対応した値を取り出す
 
-print(car.keys())
-print(car.values())
+print(car.keys()) # キーの一覧
+print(car.values()) # 値の一覧
 print(car.items()) # キーと値の一覧
+
+for k, v in car.items(): # キーがk、値がvにそれぞれ格納される
+    print('key = {}, value = {}'.format(k,v))
+
+if 'brand' in car:
+    print('carのブランドは{}'.format(car['brand'])) # brandというキーが存在する場合だけ、処理を実行できる
+
---
+++ base11.py
@@ -0,0 +1,8 @@
+# 辞書型のメソッド
+
+car = {'brand': 'Toyota', 'model': 'Prius', 'year': 2015}
+
+tmp_car = {'country': 'Japan', 'prefecture': 'Aichi', 'model': 'カローラ'}
+
+car.update(tmp_car) # 追加、更新
+print(car)
